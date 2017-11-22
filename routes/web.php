c2be139451acc4f9da2f8968cf14aabbef8b3045
samyao93 <samyao93@outlook.com>
2017-11-22 15:23:43 +0100

    public\css\bootstrap.css

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,45 @@
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('index');
+});
+
+/*Frontend views*/
+ 
+
+Route::get('/contact', function () {
+    return view('frontend.contact');
 });
 
 
+Route::get('/login', function () {
+    return view('frontend.auth.login');
+});
+
+Route::get('/register', function () {
+    return view('frontend.auth.register');
+});
+
+Route::get('/about', function () {
+    return view('frontend.about');
+});
+
+Route::get('/partner', function () {
+    return view('frontend.partner');
+});
+
+Route::get('/registerpartner', function () {
+    return view('frontend.auth.registerPartner');
+});
+/*-------------Hashing ID for matricule*----------------*/
+
+Route::bind('id', function ($id) {
+    return Hasher::decode($id);
+});
+
+// GET /users/3kTMd
+Route::get('/users/{id}', function($id) {
+    // $id is now an int
+    return User::find($id);
+});
  
